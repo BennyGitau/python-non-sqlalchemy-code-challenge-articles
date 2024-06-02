dd2705dbffec2d86df19dbee93fb114baa2b73ea
BensonGitau <bensonkamaugitau@gmail.com>
2024-06-02 09:59:45 +0300

    complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,124 @@
+from collections import Counter
+
 class Article:
+    all = []
+    
     def __init__(self, author, magazine, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be of type str")
+        if not (5 <= len(title) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters")
+        self._title = title
+
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("Author must be of type Author")
+        self._author = value
+    
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise TypeError("Magazine must be of type Magazine")
+        self._magazine = value
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name,str):
+            raise TypeError("Name must be of type str")
+        if len(name) == 0:
+            raise ValueError('Name must be longer than 0 characters')
+        self._name = name
 
+    @property
+    def name(self):
+        return self._name
+    
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
-
+        return list(set(article.magazine for article in self.articles()))
+        
     def add_article(self, magazine, title):
-        pass
-
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be of type Magazine.")
+        return Article(self, magazine, title)
+        
     def topic_areas(self):
-        pass
+        mag_categories = [article.magazine.category for article in self.articles()]
+        if mag_categories:
+            return list(set(mag_categories))
+        return None
 
 class Magazine:
+    magazines = []
     def __init__(self, name, category):
         self.name = name
-        self.category = category
+        self._category = category
+        Magazine.magazines.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value,str) and (2 <= len(value) <=16):
+            self._name = value
+        else:
+            raise ValueError('Name must be of type string and between 2 and 16 characters')
+        
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) != 0:
+            self._category = value
+        else:
+            raise ValueError("Category must be type string with more than 0 characters")
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        if titles:
+            return titles
+        return None
 
     def contributing_authors(self):
-        pass
+        authors = Counter(article.author for article in self.articles())
+        result = [author for author, count in authors.items() if count > 2]
+        return result if result else None
+    
+    @classmethod
+    def top_publisher(cls):
+        if not cls.magazines:
+            return None
+        top_magazine = max(cls.magazines, key=lambda mag: len(mag.articles()))
+        return top_magazine if top_magazine.articles() else None
+
+    
+        
